cde0e884427d36bef8daf7bd5b6fd1b54586843c
Кожекина Юлия <muza614@yandex.ru>
2022-06-09 14:00:30 +0300

    практика 3

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'ru-ru'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
